d6a5177f38d5f6cbf58ca72d50685e525e256ab3
Sturla Þorvaldsson <sturla76@hotmail.com>
2018-06-17 22:11:22 +0000

    Example6: Memoize

---
+++ Sturla.io.Func.Six.Console/Caching.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Serilog;
+
+namespace Sturla.io.Func.Six.Console
+{
+	/// <summary>
+	/// Memoization take a look at https://www.infoq.com/news/2007/01/CSharp-memory
+	/// </summary>
+	public static class Caching
+	{
+		/// <summary>
+		/// Here you do recursion, but you remember the value of each Fibonacci/LowerCasing. 
+		/// Thus, instead of 29 Million for 35th Fibonacci you do 37 calculations. And it takes just 0.002 seconds.
+		/// 
+		/// "In computing, memoization or memoisation is an optimization technique used primarily to speed up computer programs 
+		/// by storing the results of expensive function calls and returning the cached result when the same inputs occur again."
+		/// https://en.wikipedia.org/wiki/Memoization
+		/// </summary>
+		/// <typeparam name="TArgument"></typeparam>
+		/// <typeparam name="TResult"></typeparam>
+		/// <param name="function"></param>
+		/// <returns></returns>
+		public static Func<TArgument, TResult> Memoize<TArgument, TResult>(this Func<TArgument, TResult> function)
+		{
+			var methodDictionaries = new Dictionary<string, Dictionary<TArgument, TResult>>();
+
+			var name = function.Method.Name;
+			if (!methodDictionaries.TryGetValue(name, out Dictionary<TArgument, TResult> values))
+			{
+				values = new Dictionary<TArgument, TResult>();
+
+				Log.Information("MethodName: {name}, ReturnType: {returnType}", name, function.Method.ReturnType);
+
+				methodDictionaries.Add(name, values);
+			}
+
+			return a =>
+			{
+				// If we have already got value cached from e.g Fibonacci(i) or  it has been added to values
+				// and there is no need to call the expensive function. We have the result
+				if (!values.TryGetValue(a, out TResult value))
+				{
+					value = function(a);
+					values.Add(a, value);
+				}
+
+				return value;
+			};
+		}
+	}
+}
---
+++ Sturla.io.Func.Six.Console/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Serilog;
+
+namespace Sturla.io.Func.Six.Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Logging
+			Log.Logger = new LoggerConfiguration()
+			.MinimumLevel.Verbose()
+			.WriteTo.Console()
+			.CreateLogger();
+			#endregion
+
+			// Set up 
+			// Wraps the Dictionary<TArgument, TResult> dictionary and the original function into a NEW function, 
+			//that gets returned from the Meoize method).
+			Fibonacci = Fibonacci.Memoize();
+			SuperComplexLowerCaseing = SuperComplexLowerCaseing.Memoize();
+
+			// First run of two
+			for (int i = 0; i < 50; i++)
+				Log.Information("{fib}", Fibonacci(i));
+
+			// Some other super complex and time consuming lowercasing.
+			Log.Information("{lower}", SuperComplexLowerCaseing("Test"));
+			Log.Information("{lower}", SuperComplexLowerCaseing("Test"));
+			Log.Information("{lower}", SuperComplexLowerCaseing("Not"));
+			Log.Information("{lower}", SuperComplexLowerCaseing("Test"));
+
+			// Now notice that everyone of these are fetched from the Dictionary of values calculated in the first run.
+			for (int i = 0; i < 50; i++)
+				Log.Information("{fib}", Fibonacci(i));
+
+
+			System.Console.ReadKey();
+		}
+
+		public static Func<int, int> Fibonacci = (n) =>
+		{
+			return n > 1 ? Fibonacci(n - 1) + Fibonacci(n - 2) : n;
+		};
+
+		public static Func<string, string> SuperComplexLowerCaseing = (n) =>
+		{
+			// I just didn't come up with anything more clever. Need to find my old Calculus books.
+			return n.ToLower();
+		};
+	}
+}
